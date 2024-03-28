f7a17823df96e1869edd4574933d21f4e7e389e3
yudhawan <yudhaashter@gmail.com>
2024-03-28 11:33:13 +0700

    adding midleware

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+const allowOrigin = process.env.NODE_ENV === 'production' ? [process.env.BASE_URL, 'https://admin-intai-mall.vercel.app/', 'http://localhost:3000'] : ['http://localhost:3000']
+export function middleware(request: Request) {
+    const res = NextResponse.next()
+    const origin = request.headers.get('origin') || ''
+    if (origin && !allowOrigin.includes(origin)) return new NextResponse(null, {
+        status: 400,
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    })
+    // add the CORS headers to the response
+    // res.headers.append('Access-Control-Allow-Credentials', "true")
+    // res.headers.append('Access-Control-Allow-Origin', 'http://localhost:3000') // replace this your actual origin
+    // res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+    // res.headers.append(
+    //     'Access-Control-Allow-Headers',
+    //     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    // )
+
+    return res
+}
+
+// specify the path regex to apply the middleware to
+export const config = {
+    matcher: '/api/:path*',
+}
