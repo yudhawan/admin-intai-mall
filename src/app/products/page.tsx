588ba42348056151fffceb61157efde4f7b55f52
yudhawan <yudhaashter@gmail.com>
2024-07-11 12:50:24 +0700

    updated

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -5,7 +5,7 @@ import { getCategories, getProducts } from '../api/libsServer/serverServices'
 async function ProductPage() {
   const getAllProducts = await getProducts()
   const getAllCategories = await getCategories()
-  return <ProductsContainer getAllProducts={getAllProducts || []} getAllCategories={getAllCategories || []} />
+  return <ProductsContainer getAllProducts={getAllProducts} getAllCategories={getAllCategories} />
 }
 
 export default ProductPage
