6f01d9d0842deb1c4545639cf7733e6dbe5a33fa
yudhawan <yudhaashter@gmail.com>
2024-07-21 22:03:18 +0700

    new crypt

--- src/app/api/routes/doLogin/route.ts
+++ src/app/api/routes/doLogin/route.ts
@@ -3,13 +3,15 @@ import { res } from "../../libsServer/argApi"
 import prisma from "@/lib/prisma"
 // @ts-ignore
 import bcrypt from 'bcrypt'
+import { compareSync } from "bcrypt-ts";
 import { encryptJWT } from "@/lib/tokenServices"
 import { private_scrt } from "../../libsServer/encrypt"
 export const POST =async (req: NextRequest)=>{
     try {
         const data = await req.json()
         const getPass = await prisma.admin.findFirst({where:{username:data?.username}})
-        const checkPass = await bcrypt.compare(data.password,getPass?.password)
+        // const checkPass = await bcrypt.compare(data.password,getPass?.password)
+        const checkPass = compareSync(data?.password,getPass?.password||"")
         if (checkPass) {
             const token = await encryptJWT({payload:{username:getPass?.username},scrt:private_scrt})
             await prisma.admin.update({where:{id:getPass?.id},data:{token:token}})
